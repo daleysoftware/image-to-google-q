0400c1aeb622c6fdd5675afe7107064a8c66d499
Matt Pillar <matt@aerofs.com>
2018-02-02 14:33:22 -0800

    Add search and demo video

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 import pytesseract
 import sys
 from PIL import Image, ImageEnhance, ImageFilter
+from googleapiclient.discovery import build
+import pprint
+
+
+API_KEY = "<redacted>"
+CSE_ID = "<redacted>"
+
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    results = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return results['items'][0]
 
 
 TMP_IMG = '/tmp/image-to-google-q.png'
 
 
 def main(image_filename):
+    # Parse text
     im = Image.open(image_filename)
     im = im.filter(ImageFilter.MedianFilter())
     enhancer = ImageEnhance.Contrast(im)
@@ -14,7 +27,11 @@ def main(image_filename):
     im = im.convert('1')
     im.save(TMP_IMG)
     text = pytesseract.image_to_string(Image.open(TMP_IMG))
-    print(text)
+    print('Parsed text: ' + text)
+
+    # Query google
+    results = google_search(text, API_KEY, CSE_ID, num=10)
+    pprint.pprint(results)
 
 
 if __name__ == '__main__':
